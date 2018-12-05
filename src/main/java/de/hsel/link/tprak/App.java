f5f4cd52be8f103a055e4afa364ff132455ff07c
Arne Otten <aotten@technik-emden.de>
2018-12-06 00:19:58 +0100

    neues projekt

---
+++ src/main/java/de/hsel/link/tprak/App.java
@@ -0,0 +1,13 @@
+package de.hsel.link.tprak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
